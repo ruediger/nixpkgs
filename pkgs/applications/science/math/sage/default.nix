f70d34034199d1ea8cab306853dc754ec703b9b8
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2014-08-19 23:01:38 +0200

    sage: Try to fix.
    Fails while building python... why is it trying to build its own python?
    Sage packaging is just messed up...

--- pkgs/applications/science/math/sage/default.nix
+++ pkgs/applications/science/math/sage/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, m4, perl, gfortran, texLive, ffmpeg, tk
+{ stdenv, fetchurl, bash, m4, perl, gfortran, texLive, ffmpeg, tk
 , imagemagick, liblapack
 }:
 
@@ -10,12 +10,20 @@ stdenv.mkDerivation rec {
     sha256 = "bd317407271a7c8b94c1ff65b48b36ef9265685033005d5f748cc0c348e75033";
   };
 
-  buildInputs = [ m4 perl gfortran texLive ffmpeg tk imagemagick liblapack ];
+  buildInputs = [ bash m4 perl gfortran texLive ffmpeg tk imagemagick liblapack ];
 
   enableParallelBuilding = true;
 
   preConfigure = ''
     export SAGE_NUM_THREADS=$NIX_BUILD_CORES
+    find . -type f -a -executable -exec \
+      sed -i -e 's,^\#! */usr/bin/env bash,\#!${bash}/bin/bash,' \
+             -e 's,^\#! */usr/bin/env sh,\#!${bash}/bin/bash,' \
+             -e 's,^\#! */bin/sh,\#!${bash}/bin/bash,' \
+             -e 's,^\#! */bin/dash,\#!${bash}/bin/bash,'
+      {} +
+#             -e 's,^\#! */usr/bin/env python,\#!${python}/bin/${python.executable},' \
+
     sed -i 's/if ! [ -d "$HOME" ]/if [ -d "$HOME" ]/' src/bin/sage-env
   '' + stdenv.lib.optionalString stdenv.isDarwin ''
     sed -i "s/ld_version = try_run('ld  -v')/ld_version = 'Apple'/" \
